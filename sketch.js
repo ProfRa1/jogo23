691b7fb6e4c3f26ecd684a1113190f165c3825c4
ProfRa1 <96995750+ProfRa1@users.noreply.github.com>
2023-05-27 11:39:33 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,65 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world, backgroundImg;
+var canvas, angle, tower, ground, cannon;
+var cannonBall;
+
+var nomes = ["Ana", "Maria", "João"];
+//matriz 1D
+var balls2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var balls3 = [
+    [1, 2],
+    [3, 4],
+    [5, 6],
+    [7, 8, 9],
+];
+
+console.log(balls2);
+
+console.log(balls3);
+console.log(balls3[0][0]);
+
+function preload() {
+    backgroundImg = loadImage("./assets/background.gif");
+    towerImage = loadImage("./assets/tower.png");
+}
+
+function setup() {
+    canvas = createCanvas(1200, 600);
+    engine = Engine.create();
+    world = engine.world;
+
+    angleMode(DEGREES);
+    angle = 15;
+
+    ground = Bodies.rectangle(0, height - 1, width * 2, 1, { isStatic: true });
+    World.add(world, ground);
+
+    tower = Bodies.rectangle(160, 350, 160, 310, { isStatic: true });
+    World.add(world, tower);
+    cannon = new Cannon(180, 110, 130, 100, angle);
+}
+
+function draw() {
+    background(189);
+    image(backgroundImg, 0, 0, width, height);
+
+    Engine.update(engine);
+
+    push();
+    fill("brown");
+    rectMode(CENTER);
+    rect(ground.position.x, ground.position.y, width * 2, 1);
+    pop();
+
+    push();
+    imageMode(CENTER);
+    image(towerImage, tower.position.x, tower.position.y, 160, 310);
+    pop();
+
+    cannon.display();
+}
